97335be816dd4391044ec014183199cfa19ab24c
DroidDaemon <sandeepthakur89@gmail.com>
2018-01-04 21:19:34 +0530

    Integration of DEXTER Library( simplifies the process of requesting permissions at runtime)

---
+++ app/src/main/java/com/example/sandeepsingh/camera2tute/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.sandeepsingh.camera2tute;
+
+import android.Manifest;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.sandeepsingh.cameratute1.R;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initDexter();
+
+    }
+
+    private void initDexter() {
+        Dexter.withActivity(this).withPermission(Manifest.permission.CAMERA)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+
+                    }
+                });
+    }
+}
